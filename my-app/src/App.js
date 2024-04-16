b55c752888af927aa7981112c44731181d73582e
Phoebe5524 <113878882+Phoebe5524@users.noreply.github.com>
2024-04-16 21:46:49 +0000

    create react app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import bookClass from "./components/problems/bookClass";
+import chooseDate from "./components/problems/chooseDate";
+import motivateQuotes from "./components/problems/motivateQuotes";
+import upcomingClass from "./components/problems/upcomingClass";
+
+function App() {
+  return (
+    <>
+
+    </>
+  );
+  
+}
+
+export default App;
