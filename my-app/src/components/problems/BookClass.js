53fc60c9f308802f93f0a20b0cbd7fafbfbe4592
Phoebe5524 <113878882+Phoebe5524@users.noreply.github.com>
2024-04-18 19:56:33 +0000

    bookclass

--- my-app/src/components/problems/BookClass.js
+++ my-app/src/components/problems/BookClass.js
@@ -1,23 +1,42 @@
-// components/BookClass.js
 import React from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
-import classesData from '../util/class';  
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import classesData from '../util/class';
 
 const BookClass = () => {
+    // Handler for when a class card is clicked
+    const handleClassClick = (cls) => {
+        // You can handle the click event here, for example, open a modal with class details or navigate to a class-specific page
+        console.log(`Class clicked: ${cls.id}`);
+    };
+
     return (
         <section className="class-booking">
             <h2>Class Dashboard</h2>
-            <Tabs defaultActiveKey="class1" id="class-tabs">
-                {classesData.map((cls, index) => (
-                    <Tab eventKey={`class${cls.id}`} title={cls.class} key={cls.id}>
-                        <div className="p-3">
-                            <h3>{cls.class}</h3>
-                            <p>Type: {cls.type}</p>
-                            <p>Coach: {cls.coach}</p>
+            <div className="class-cards-container">
+                {classesData.map((cls) => (
+                    <button 
+                        className="class-card" 
+                        key={cls.id} 
+                        onClick={() => handleClassClick(cls)}
+                    >
+                        <div className="class-card-content">
+                            <div className='class-card-content-image'>
+                            <img src={cls.image} alt={cls.name} style={{ width: '250px', height: '166.66px'}} />
+                            </div>
+                            <div className='class-card-content-text'>
+                                <h3>{cls.class}</h3>
+                                <p>Type: {cls.type}</p>
+                                <p>Coach: {cls.coach}</p>
+                                <Link to={`/class/${cls.id}`}>
+                                    <Button variant="primary">Go to Calendar</Button>
+                                </Link>
+                            </div>
+
                         </div>
-                    </Tab>
+                    </button>
                 ))}
-            </Tabs>
+            </div>
         </section>
     );
 };
